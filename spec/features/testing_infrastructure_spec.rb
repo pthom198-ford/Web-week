771dab1d9914854f7d3a47ce07d141366621e518
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-30 13:24:29 +0100

    web-week pairing with pcole

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,3 +6,8 @@ scenario 'Can run app and check page content' do
   expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
+
+# capybara can do save_and_open_page
+#unit test testing small encapsultated code, just ruby based model
+#test integration and how code interacts
