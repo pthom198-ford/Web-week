4faf09be162d7749c505bcb29583c3eef5e589f3
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-30 20:32:19 +0100

    post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     "Testing infrastructure working!"
   end
 
   post '/names' do
     p params
-    @user_name1 = params[:user_name1]
-    @user_name2 = params[:user_name2]
-    erb(:play)
+    session[:player1] = params[:user_name1]
+    session[:player2] = params[:user_name2]
+    redirect '/play'
+    # erb(:play)
     # "#{@user_name1} <br> #{@user_name2}"
     # erb :form
   end
@@ -18,6 +22,11 @@ class Battle < Sinatra::Base
     erb(:form)
   end
 
+  get '/play' do
+    @user_name1 = session[:player1]
+    @user_name2 = session[:player2]
+      erb :play
+  end
   # start the server if ruby file executed directly
 
   run! if app_file == $0
