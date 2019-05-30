d963800dfc47763519513eca7c14dee5d880b90d
Piers Thompon <pthom198@MGC000009767.warley.ford.com>
2019-05-30 17:13:59 +0100

    pairing progress with peter

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ class Battle < Sinatra::Base
   get '/' do
     "Testing infrastructure working!"
   end
+
+  post '/names' do
+    p params
+    @user_name1 = params[:user_name1]
+    @user_name2 = params[:user_name2]
+    erb(:play)
+    # "#{@user_name1} <br> #{@user_name2}"
+    # erb :form
+  end
+
+  get '/players' do
+    erb(:form)
+  end
+
   # start the server if ruby file executed directly
+
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,12 +1,38 @@
 # require '../app.rb'
 
 feature 'Testing infrastructure' do
-scenario 'Can run app and check page content' do
-  visit('/')
-  expect(page).to have_content 'Testing infrastructure working!'
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
   end
 end
 
+feature 'Entering Players' do
+scenario 'can see and click submit button' do
+  visit('/players')
+  expect(page).to have_button('Submit')
+end
+
+scenario 'can fill in the form with first name' do
+  visit('/players')
+  # fill_in "First Name", with: "Peter"
+  expect(page).to have_field('Player One')
+end
+scenario 'can fill in the form with first name' do
+  visit('/players')
+  # fill_in "First Name", with: "Peter"
+  expect(page).to have_field('Player Two')
+end
+
+scenario 'can see the names displayed once submitted' do
+  visit('/players')
+  fill_in 'Player One', with: 'Peter'
+  fill_in 'Player Two', with: 'Piers'
+  click_button('Submit')
+  expect(page).to have_text('Peter')
+  expect(page).to have_text('Piers')
+end
+end
 
 # capybara can do save_and_open_page
 #unit test testing small encapsultated code, just ruby based model
